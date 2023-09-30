1650b852ce95d0cb6446f9badd7e74b92b7c2df8
Minh Ngo <ngom@ufl.edu>
2023-09-29 23:45:31 -0400

    added original dataset and finished querying all necessary tables from database

--- Queries.sql
+++ Queries.sql
@@ -23,7 +23,7 @@ GROUP BY
 ORDER BY months;
 
 
--- age range relation ship with product category
+-- age range relation ship with product category and total revenue per category
 
 SELECT
 	CASE
@@ -38,9 +38,9 @@ SELECT
         WHEN age between 90 AND 99 THEN '90s'
 		ELSE 'other'
 	END AS age_range,
-    product_category as category,
-    SUM(total_amount),
-    SUM(quantity)
+    product_category AS category,
+    SUM(total_amount) AS total_amount_per_category,
+    SUM(quantity) AS quantity_per_category
 FROM 
 	retail_sales_dataset
 GROUP BY 
@@ -79,4 +79,51 @@ SELECT
     SUM(quantity_sum) AS total_quantity_sum
 FROM
     AgeCategorySums;
+    
+    
+-- Distribution of customers by gender and age
 
+SELECT
+	CASE
+		WHEN age between 15 AND 19 THEN '15-19'
+        WHEN age between 20 AND 29 THEN '20s'
+        WHEN age between 30 AND 39 THEN '30s'
+        WHEN age between 40 AND 49 THEN '40s'
+        WHEN age between 50 AND 59 THEN '50s'
+        WHEN age between 60 AND 69 THEN '60s'
+        WHEN age between 70 AND 79 THEN '70s'
+        WHEN age between 80 AND 89 THEN '80s'
+        WHEN age between 90 AND 99 THEN '90s'
+		ELSE 'other'
+	END AS age_range,
+    gender,
+    SUM(total_amount) AS total_amount_per_age_and_gender,
+    SUM(quantity) AS quantity,
+    product_category
+FROM
+	retail_sales_dataset
+GROUP BY 
+	age_range, gender, product_category
+ORDER BY
+	age_range ASC;
+
+-- Most popular product categories for each gender
+SELECT 
+    gender,
+    (SELECT product_category 
+     FROM retail_sales_dataset t2 
+     WHERE t1.Gender = t2.Gender 
+     GROUP BY Gender, product_category 
+     ORDER BY COUNT(*) DESC 
+     LIMIT 1) AS MostPopularCategory
+FROM 
+    retail_sales_dataset t1
+GROUP BY 
+    gender;
+
+-- avg basket size
+SELECT
+	AVG(total_amount) AS average_amount,
+    AVG(quantity) AS average_quantity
+FROM retail_sales_dataset;
+    
